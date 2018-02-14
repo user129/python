3d850ed4fd824cc199ca23ac56ea3537924a926f
user129 <33879349+user129@users.noreply.github.com>
2018-02-14 15:48:00 +0200

    Update ergasia4.py

--- ergasia4.py
+++ ergasia4.py
@@ -1,37 +1,18 @@
-join=True
-'''words = {} convert an integer number into words'''
-num=int(input("give a number to convert to english(999.999.999<):"))
-units = ['','one','two','three','four','five','six','seven','eight','nine']
-teens = ['','eleven','twelve','thirteen','fourteen','fifteen','sixteen', \
-             'seventeen','eighteen','nineteen']
-tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy', \
-            'eighty','ninety']
-thousands = ['','thousand','million']
-                 
-words = []
+num_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+num=input()
 
-if num==0: words.append('zero')
-else:
-   numStr = '%d'%num
-   numStrLen = len(numStr)
-   groups = int((numStrLen+2)/3)
-   
-   numStr = numStr.zfill(groups*3)
-   for i in range(0,groups*3,3):
-        h,t,u = int(numStr[i]),int(numStr[i+1]),int(numStr[i+2])
-        g = int(groups-(i/3+1))
-        if h>=1:
-            words.append(units[h])
-            words.append('hundred')
-        if t>1:
-            words.append(tens[t])
-            if u>=1: words.append(units[u])
-        elif t==1:
-            if u>=1: words.append(teens[u])
-            else: words.append(tens[t])
-        else:
-            if u>=1: words.append(units[u])        
-        if (g>=1) and ((h+t+u)>0): words.append(thousands[g]+',')
-   if join: print( ' '.join(words)) 
-   print (words)
+def num2roman(num):
+
+    roman = ''
+
+    while num > 0:
+        for i, r in num_map:
+            while num >= i:
+                roman += r
+                num -= i
+
+    return roman
+ 
+print(num2roman(num))
    
